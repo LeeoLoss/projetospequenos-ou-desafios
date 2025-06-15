b8cf8201c115be5f0f48a8bf4d8e130a42871432
Leonardo Loss <132675221+LeeoLoss@users.noreply.github.com>
2025-06-15 00:39:01 -0300

    Update circular-lawn-calculator.py

--- python-data-science-projects/circular-lawn-calculator.py
+++ python-data-science-projects/circular-lawn-calculator.py
@@ -1,8 +1,11 @@
+# Importando a bibiloteca e seus métodos
 from math import pi, pow
 
 raio = float(input('Digite o raio da área circular em metros: '))
 
+# Calculando a área e o custo em reais com os métodos da biblioteca math
 area = pi * pow(raio, 2)
 valor = area * 25.0
 
+# Exibindo o cálculo e custo na tela
 print(f'Você vai precisar pagar R${round(valor, 2)} por uma área de {round(area, 2)} metros de grama.')
